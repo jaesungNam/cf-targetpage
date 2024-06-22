c1452f39026b7f89ee97ce80e2f0dcc8244b3fae
Jaesung Nam <overpowerman@gmail.com>
2024-06-22 15:36:23 +0900

    api 추가

---
+++ src/app/api/user/route.ts
@@ -0,0 +1,21 @@
+
+
+export async function GET() {
+
+  return Response.json({ ok: 'get' })
+}
+
+export async function POST() {
+
+  return Response.json({ ok: 'post' })
+}
+
+export async function PUT() {
+
+  return Response.json({ ok: 'put' })
+}
+
+export async function DELETE() {
+
+  return Response.json({ ok: 'delete' })
+}
